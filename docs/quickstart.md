d7e8f6dd2ee534fad10a0ba1b7fdcec3a6917b3b
Younes Strittmatter <younes_strittmatter@brown.edu>
2023-09-02 10:04:24 -0400

    docs: sampler to experimentalist at the appropriate places

--- docs/quickstart.md
+++ docs/quickstart.md
@@ -4,7 +4,7 @@ You will need:
 
 - `python` 3.8 or greater: [https://www.python.org/downloads/](https://www.python.org/downloads/)
 
-*leverage is a part of the `autora` package:
+*leverage experimentalist* is a part of the `autora` package:
 
 ```shell
 pip install -U autora["experimentalist-leverage"]
--- src/autora/experimentalist/leverage/__init__.py
+++ src/autora/experimentalist/leverage/__init__.py
@@ -8,46 +8,46 @@ def sample(
 ):
     """
 
-    # The Leverage Sampler
+    # The Leverage Experimentalist
 
-    This sampler uses the statistical concept of leverage by refitting the provided models
+    This experimentalist uses the statistical concept of leverage by refitting the provided models
     iteratively with the leave-one-out method.
 
     ---
     WARNING:
-    This sampler needs to fit each model you provide it n times, where n corresponds to the
+    This experimentalist needs to fit each model you provide it n times, where n corresponds to the
     number of datapoints you have.
-    As such, the computational time and power needed to run this sampler increases exponentially
-    with increasing number of models and datapoints.
+    As such, the computational time and power needed to run this experimentalist increases
+    exponentially with increasing number of models and datapoints.
 
     ---
 
     In each iteration, it computes the degree to which the currently removed datapoint
         has influence on the model.
     If the model remains stable, the datapoint is deemed to have little influence on the model,
-        and as such will have a low likelyhood of being selected for further investigation.
+        and as such will have a low likelihood of being selected for further investigation.
     In contrast, if the model changes, the datapoint is influential on the model,
         and has a higher likelihood of being selected for further investigation.
 
-    Specifically, you provide the sampler with a model that has been trained on all of the data.
-    On each iteration, the sampler fits a new model with all data aside from one datapoint.
-    Both models then predict Y scores from the original X variable and compute a mean squared error
-    (MSE) for each X score.
+    Specifically, you provide the experimentalist with a model that has been trained on all of
+    the data. On each iteration, the experimentalist fits a new model with all data aside from one
+    datapoint. Both models then predict Y scores from the original X variable and compute a mean
+    squared error (MSE) for each X score.
 
-    The sampler then computes a ratio of the MSE scores between the sampler model
+    The experimentalist then computes a ratio of the MSE scores between the experimentalist model
         and the original model that you provided:
 
     As such, values above one indicates that the original model fit the data better
-        than the sampler model when removing that datapoint.
-    In contrast, values below one indicates that the sampler model fit the data better
+        than the experimentalist model when removing that datapoint.
+    In contrast, values below one indicates that the experimentalist model fit the data better
         than the original model when removing that datapoint.
     And a value of one indicates that both models fit the data equally.
         If you provide multiple models, it will then average across these models to result
         in an aggregate MSE score for each X score. In the future,
         it might be a good idea to incorporate multiple models in a more sophisticated way.
 
-    Finally, the sampler then uses these aggregated ratios to select the next set of datapoints
-        to explore in one of three ways, declared with the 'fit' parameter.
+    Finally, the experimentalsit then uses these aggregated ratios to select the next set of
+    datapoints to explore in one of three ways, declared with the 'fit' parameter.
         -'increase' will choose samples focused on X scores where the fits got better
             (i.e., the smallest MSE ratios)
         -'decrease' will choose samples focused on X scores where the fits got worse
